3b074af3a6f7d0bdca9fcad2025bbb2f90e5c702
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-05 21:27:08 +0600

    create useAuth hooks

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,3 +1,10 @@
-export default function MyApp() {
-  return <></>;
+import { useContext, useDebugValue } from "react";
+
+import { AuthContext } from "../context";
+
+export default function useAuth() {
+  const [auth] = useContext(AuthContext);
+  useDebugValue(auth, (auth) => (auth?.user ? "Logged In" : "Logged Out"));
+
+  return useContext(AuthContext);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import AuthProvider from "./provider/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </StrictMode>
 );
