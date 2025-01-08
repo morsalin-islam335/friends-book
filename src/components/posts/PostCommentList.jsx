81da1ba8c7170022eb71833bbb634aff2148637f
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-08 18:41:01 +0600

    complete PostCommentList component

--- src/components/posts/PostCommentList.jsx
+++ src/components/posts/PostCommentList.jsx
@@ -1,7 +1,29 @@
-export default function PostCommentList() {
+// eslint-disable-next-line react/prop-types
+export default function PostCommentList({ comments }) {
   return (
     <>
-      <h1>PostCommentList</h1>
+      <div className="space-y-4 divide-y divide-lighterDark pl-2 lg:pl-3">
+        {comments &&
+          // eslint-disable-next-line react/prop-types
+          comments.map((comment) => (
+            <div className="flex items-center gap-3 pt-4" key={comment.id}>
+              <img
+                className="max-w-6 max-h-6 rounded-full"
+                src={`${import.meta.env.VITE_SERVER_BASE_URL}/${
+                  comment?.author?.avatar
+                }`}
+                alt="avatar"
+              />
+
+              <div>
+                <div className="flex gap-1 text-xs lg:text-sm">
+                  <span>{comment?.author?.name}</span>
+                  <span>{comment?.comment}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </>
   );
 }
