72021f9514ff9bc43f4dc31b226e48bf3df872cc
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-09 07:00:56 +0600

    fixed logout issue

--- src/components/auth/LogOut.jsx
+++ src/components/auth/LogOut.jsx
@@ -1,10 +1,13 @@
 import LogOutIcon from "../../assets/icons/logout.svg";
 
 import { useNavigate } from "react-router-dom";
+import useAuth from "./../../hooks/useAuth";
 
 const LogOut = () => {
   const navigate = useNavigate();
+  const { setAuth } = useAuth();
   const handleLogout = () => {
+    setAuth({}); // set auth with an empty object.
     navigate("/login");
   };
   return (
