92e335aba918ca3ace5e180028780799bcba18c1
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-10 00:35:13 +0600

    implement create post feature

--- src/components/posts/NewPost.jsx
+++ src/components/posts/NewPost.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import useAuth from "../../hooks/useAuth";
+import PostEntry from "./PostEntry";
 
 export default function NewPost() {
   const [showPostEntry, setShowPostEntry] = useState(false);
@@ -7,27 +8,31 @@ export default function NewPost() {
   const { auth } = useAuth();
   return (
     <>
-      <div className="card">
-        <div className="flex-center mb-3 gap-2 lg:gap-4">
-          <img
-            className="max-w-10 max-h-10 rounded-full lg:max-h-[58px] lg:max-w-[58px]"
-            src={`${import.meta.env.VITE_SERVER_BASE_URL}/${
-              auth?.user?.avatar
-            }`}
-            alt="avatar"
-          />
+      {showPostEntry ? (
+        <PostEntry />
+      ) : (
+        <div className="card">
+          <div className="flex-center mb-3 gap-2 lg:gap-4">
+            <img
+              className="max-w-10 max-h-10 rounded-full lg:max-h-[58px] lg:max-w-[58px]"
+              src={`${import.meta.env.VITE_SERVER_BASE_URL}/${
+                auth?.user?.avatar
+              }`}
+              alt="avatar"
+            />
 
-          <div className="flex-1">
-            <textarea
-              className="h-16 w-full rounded-md bg-lighterDark p-3 focus:outline-none sm:h-20 sm:p-6"
-              name="post"
-              id="post"
-              placeholder="What's on your mind?"
-              onClick={() => setShowPostEntry(true)}
-            ></textarea>
+            <div className="flex-1">
+              <textarea
+                className="h-16 w-full rounded-md bg-lighterDark p-3 focus:outline-none sm:h-20 sm:p-6"
+                name="post"
+                id="post"
+                placeholder="What's on your mind?"
+                onClick={() => setShowPostEntry(true)}
+              ></textarea>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
