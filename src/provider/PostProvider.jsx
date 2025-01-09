1f8efd7ba8047a5b712289e4b36856081a08ec93
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-09 21:25:05 +0600

    complete postProvider

--- src/provider/PostProvider.jsx
+++ src/provider/PostProvider.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable react/prop-types */
+import { useReducer } from "react";
 import { PostContext } from "../context";
+import { initialState, postReducer } from "../reducers/postReducer";
 
 export default function PostProvider({ children }) {
+  const { state, dispatch } = useReducer(postReducer, initialState);
   return (
     <>
-      <PostContext.Provider>{children}</PostContext.Provider>
+      <PostContext.Provider value={{ state, dispatch }}>
+        {children}
+      </PostContext.Provider>
     </>
   );
 }
