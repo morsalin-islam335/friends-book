0b87c687e79e28aac065c060a6b49403cf781350
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-09 08:45:45 +0600

    Implement Post like Feature from PostAction component and send post instead of postId from PostCard component

--- src/components/posts/PostAction.jsx
+++ src/components/posts/PostAction.jsx
@@ -1,16 +1,46 @@
+/* eslint-disable react/prop-types */
 import CommentIcon from "../../assets/icons/comment.svg";
 
+import LikeFilledIcon from "../../assets/icons/like-filled.svg";
 import LikeIcon from "../../assets/icons/like.svg";
 
+import { useState } from "react";
 import ShareIcon from "../../assets/icons/share.svg";
+import useAuth from "./../../hooks/useAuth";
+import useAxios from "./../../hooks/useAxios";
 
-// eslint-disable-next-line react/prop-types
-export default function PostAction({ postId, commentCount }) {
+export default function PostAction({ post, commentCount }) {
+  const { api } = useAxios();
+  const { auth } = useAuth();
+  const [like, setLike] = useState(post?.likes?.includes(auth?.user?.id));
+
+  const handleLike = async () => {
+    try {
+      // const response = await api.patch(
+      //   `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id}/like`
+      // );
+
+      const response = await api.patch(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id}/like`
+      );
+
+      if (response.status === 200) {
+        setLike(true);
+      }
+    } catch (error) {
+      console.error(error);
+      setLike(false);
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-between py-6 lg:px-10 lg:py-8">
-        <button className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm">
-          <img src={LikeIcon} alt="Like" />
+        <button
+          className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm"
+          onClick={handleLike}
+        >
+          <img src={like ? LikeFilledIcon : LikeIcon} alt="Like" />
           <span>Like</span>
         </button>
         <button className="icon-btn space-x-2 px-6 py-3 text-xs lg:px-12 lg:text-sm">
