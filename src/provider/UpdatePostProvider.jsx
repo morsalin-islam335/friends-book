87f79c38cc7da1293b077d8ab20f1caba1a92de4
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-11 20:57:44 +0600

    create UpdatePostProvider so that we can pass edited post through this provider and after that we can set new post data with that provider data

---
+++ src/provider/UpdatePostProvider.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { UpdatePostContext } from "../context";
+
+export default function UpdatePostProvider({ children }) {
+  const [updatePost, setUpdatePost] = useState(null);
+
+  return (
+    <>
+      <UpdatePostContext.Provider value={(updatePost, setUpdatePost)}>
+        {children}
+      </UpdatePostContext.Provider>
+    </>
+  );
+}
