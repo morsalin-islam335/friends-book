775f336d6142c6e6acf1a74c0817e6d518de6666
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-11 20:48:13 +0600

    successfully handle file upload feature from new post

--- src/components/posts/PostEntry.jsx
+++ src/components/posts/PostEntry.jsx
@@ -1,5 +1,3 @@
-import { useForm } from "react-hook-form";
-
 import useAxios from "./../../hooks/useAxios";
 import usePost from "./../../hooks/usePost";
 import { useProfile } from "./../../hooks/useProfile";
@@ -7,7 +5,7 @@ import { useProfile } from "./../../hooks/useProfile";
 import { actions } from "../../actions";
 import useAuth from "./../../hooks/useAuth";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import AddPhoto from "../../assets/icons/addPhoto.svg";
 
@@ -21,12 +19,8 @@ const PostEntry = ({ onCreateOrCancel }) => {
   const fileInputRef = useRef();
 
   const user = profile?.user ?? auth?.user;
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const [file, setFile] = useState(null);
+  const [texts, setTexts] = useState("");
 
   const handlePostSubmit = async (data) => {
     console.log(data); // Debugging the form data
@@ -36,14 +30,18 @@ const PostEntry = ({ onCreateOrCancel }) => {
     try {
       // Create FormData instance
       const formData = new FormData();
+      formData.append("image", file);
+      formData.append("content", texts);
 
-      // Append text content
-      formData.append("content", data.content);
+      // // Append text content
+      // formData.append("content", data.content);
 
-      // Append photo if file is selected
-      if (fileInputRef.current?.files[0]) {
-        formData.append("photo", fileInputRef.current.files[0]);
-      }
+      // // Append photo if file is selected
+      // if (fileInputRef.current?.files[0]) {
+      //   formData.append("photo", fileInputRef.current.files[0]);
+      // }
+
+      // to do set form data globally
 
       // API call to submit post
       const response = await api.post(
@@ -70,14 +68,54 @@ const PostEntry = ({ onCreateOrCancel }) => {
     }
   };
 
-  const handleFileChange = () => {
-    console.log(fileInputRef.current.files[0]); // Log the selected file
+  const handleImageUpload = (event) => {
+    event.preventDefault(); // stop event default behavior
+    fileInputRef.current.click();
   };
 
-  const handleImageUpload = (event) => {
-    event.preventDefault();
-    fileInputRef.current.click(); // Programmatically open the file browser
-  };
+  // const updateImageDisplay = async () => {
+  //   try {
+  //     const formData = new FormData(); // create new form data object
+  //     // append the file to the form data object
+  //     // it works like key value pair
+
+  //     for (const file of fileUploadRef.current.files) {
+  //       formData.append("avatar", file);
+  //     }
+
+  //     const response = await api.post(
+  //       `${import.meta.env.VITE_SERVER_BASE_URL}/profile/${
+  //         state?.user?.id
+  //       }/avatar`,
+  //       formData
+  //     );
+
+  //     if (response.status === 200) {
+  //       dispatch({
+  //         type: actions.profile.IMAGE_UPDATED,
+  //         data: response.data,
+  //       });
+
+  //       setAuth((prevAuth) => {
+  //         return {
+  //           ...prevAuth,
+  //           user: {
+  //             ...prevAuth.user,
+  //             avatar: response.data.avatar,
+  //           },
+  //         };
+  //       });
+
+  //       console.log("Update Profile Image and data is", response.data);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     dispatch({
+  //       type: actions.profile.DATA_FETCH_ERROR,
+  //       error: error.message,
+  //     });
+  //   }
+  // };
 
   useEffect(() => {
     console.log("fileInputRef.current:", fileInputRef.current);
@@ -160,7 +198,7 @@ const PostEntry = ({ onCreateOrCancel }) => {
         </div>
       </form> */}
 
-      <form>
+      <form encType="multipart/form-data" onSubmit={handlePostSubmit}>
         <div className="mb-3 flex items-center justify-between gap-2 lg:mb-6 lg:gap-4">
           <div className="flex items-center gap-3">
             <img
@@ -179,18 +217,27 @@ const PostEntry = ({ onCreateOrCancel }) => {
             </div>
           </div>
 
-          <label
+          <button
             className="btn-primary cursor-pointer !text-gray-100"
-            htmlFor="photo"
+            onClick={handleImageUpload}
           >
             <img src={AddPhoto} alt="Add Photo" />
             Add Photo
-          </label>
-          <input type="file" name="photo" id="photo" className="hidden" />
+          </button>
+          <input
+            onChange={(event) => setFile(event.target.files[0])}
+            ref={fileInputRef}
+            type="file"
+            name="photo"
+            id="photo"
+            className="hidden"
+          />
         </div>
         {/* <!-- Post Text Input --> */}
 
         <textarea
+          value={texts}
+          onChange={(event) => setTexts(event.target.value)}
           name="post"
           id="post"
           placeholder="Share your thoughts..."
