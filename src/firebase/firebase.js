cb314d60ed4563aef212cbc111aee33cc227e5b5
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-12 19:34:34 +0600

    complete firebase.js file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  sendPasswordResetEmail,
+  signInWithPopup,
+} from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
@@ -18,4 +27,57 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const googleAuthProvider = new GoogleAuthProvider();
+
+const registrationWithEmailAndPassword = async (email, password) => {
+  try {
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = response.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const signInWithGoogle = async () => {
+  try {
+    const response = await signInWithPopup(auth, googleAuthProvider);
+    const user = response.user;
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const loginWithEmailAndPassword = async (email, password) => {
+  try {
+    const response = await signInWithEmailAndPassword(auth, email, password);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// const analytics = getAnalytics(app);
+
+export {
+  auth,
+  loginWithEmailAndPassword,
+  registrationWithEmailAndPassword,
+  sendPasswordReset,
+  signInWithGoogle,
+};
