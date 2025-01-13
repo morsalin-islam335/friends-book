9ddeddee43c16d3c3d4e7a384b3849b383d7c127
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-14 00:32:57 +0600

    add TostContaiiner from main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,24 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+
+// import { BrowserRouter as Router } from "react-router-dom";
+// import AuthProvider from "./provider/AuthProvider.jsx";
+// import ThemProvider from "./provider/ThemProvider.jsx";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <AuthProvider>
+//       <ThemProvider>
+//         <Router>
+//           <App />
+//         </Router>
+//       </ThemProvider>
+//     </AuthProvider>
+//   </StrictMode>
+// );
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -7,12 +28,18 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AuthProvider from "./provider/AuthProvider.jsx";
 import ThemProvider from "./provider/ThemProvider.jsx";
 
+// Import ToastContainer and its CSS
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <ThemProvider>
         <Router>
           <App />
+          {/* Add ToastContainer here */}
+          <ToastContainer />
         </Router>
       </ThemProvider>
     </AuthProvider>
