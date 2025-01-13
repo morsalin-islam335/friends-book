825329b799c917acbe4ee9ab36cf00dca1da0d92
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-13 23:37:10 +0600

    successfully implement image show feature after clicking an image

--- src/components/posts/PostBody.jsx
+++ src/components/posts/PostBody.jsx
@@ -1,16 +1,35 @@
 /* eslint-disable react/prop-types */
+
+import { useState } from "react";
+import ImageModal from "../modals/ImageModal";
+
 export default function PostBody({ poster, content }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const imageFile = `${import.meta.env.VITE_SERVER_BASE_URL}/${poster}`;
+
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
   return (
     <>
       <div className="border-b border-[#3F3F3F] py-4 lg:py-5 lg:text-xl">
         <p className="mb-4">{content ?? ""}</p>
         <div className="flexm flex-center justify-center overflow-hidden">
           {poster ? (
-            <img
-              className="w-1/2"
-              src={`${import.meta.env.VITE_SERVER_BASE_URL}/${poster}`}
-              alt="poster"
-            />
+            <>
+              <ImageModal
+                isOpen={isModalOpen}
+                imageFile={imageFile}
+                onClose={toggleModal}
+              />
+              <button className="w-1/2" onClick={toggleModal}>
+                <img
+                  src={`${import.meta.env.VITE_SERVER_BASE_URL}/${poster}`}
+                  alt="poster"
+                />
+              </button>
+            </>
           ) : null}
         </div>
       </div>
