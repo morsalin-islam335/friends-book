956c057052cec9d5927cb87368d86270f9dd0b8f
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-14 07:26:45 +0600

    use useLocalStorage hook instead of useState from useAuth provider and it works

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-
+/* eslint-disable react/prop-types */
 import { AuthContext } from "../context";
+import useLocalStorage from "./../hooks/useLocalStorage";
 export default function AuthProvider({ children }) {
-  const [auth, setAuth] = useState({});
+  const [auth, setAuth] = useLocalStorage("auth", {});
   return (
     <>
       <AuthContext.Provider value={{ auth, setAuth }}>
